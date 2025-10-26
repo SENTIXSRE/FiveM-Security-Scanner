cb1bf826758ee5ce9fbec4b1807d3a49cc37dc2d
Sentix <113181136+SENTIXSRE@users.noreply.github.com>
2025-10-26 17:56:00 +0100

    MainWindow.cpp hinzugefügt
    Implementierung der MainWindow-Klasse mit Fenstertitel und Größe.

---
+++ src/MainWindow.cpp
@@ -0,0 +1,11 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle("FiveM Security Scanner");
+    resize(800, 600);
+}
+
+MainWindow::~MainWindow() {
+}
